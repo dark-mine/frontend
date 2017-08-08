97dbb74ed20abb4cd81c1417c4f1b7c51d05f3be
pnaumov <ecrofeg1@gmail.com>
2017-08-09 00:55:08 +0300

    yii2 init

---
+++ controllers/ApiController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\controllers;
+
+use yii\filters\AccessControl;
+use yii\web\Controller;
+
+class ApiController extends Controller {
+	
+	/**
+	 * @inheritdoc
+	 */
+	public function behaviors() {
+		return [
+			'access' => [
+				'class' => AccessControl::className(),
+				'rules' => [
+					[
+						'allow' => true,
+						'roles' => ['@'],
+					],
+				],
+			],
+		];
+	}
+}
